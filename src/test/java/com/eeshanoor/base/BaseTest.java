16c54048192455ca9b0b413edc5f3c65a2794f39
eeshanoor <63711658+eeshanoor@users.noreply.github.com>
2026-06-18 03:37:24 +0500

    Add src/test/java/com/eeshanoor/base/BaseTest.java

---
+++ src/test/java/com/eeshanoor/base/BaseTest.java
@@ -0,0 +1,36 @@
+package com.eeshanoor.base;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
+
+public class BaseTest {
+    protected WebDriver driver;
+    protected static final String BASE_URL = "https://www.saucedemo.com";
+
+    @BeforeMethod
+    @Parameters({"browser"})
+    public void setUp(@Optional("chrome") String browser) {
+        if (browser.equalsIgnoreCase("firefox")) {
+            WebDriverManager.firefoxdriver().setup();
+            driver = new FirefoxDriver();
+        } else {
+            WebDriverManager.chromedriver().setup();
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--start-maximized");
+            driver = new ChromeDriver(options);
+        }
+        driver.get(BASE_URL);
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        if (driver != null) driver.quit();
+    }
+}
