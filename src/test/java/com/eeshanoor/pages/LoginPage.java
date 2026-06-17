5d83f4d75c715c3a64a634673d164897ee1cc497
eeshanoor <63711658+eeshanoor@users.noreply.github.com>
2026-06-18 03:37:25 +0500

    Add src/test/java/com/eeshanoor/pages/LoginPage.java

---
+++ src/test/java/com/eeshanoor/pages/LoginPage.java
@@ -0,0 +1,49 @@
+package com.eeshanoor.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import java.time.Duration;
+
+public class LoginPage {
+    private final WebDriver driver;
+    private final WebDriverWait wait;
+
+    private final By usernameField = By.id("user-name");
+    private final By passwordField = By.id("password");
+    private final By loginButton   = By.id("login-button");
+    private final By errorMessage  = By.cssSelector("[data-test='error']");
+
+    public LoginPage(WebDriver driver) {
+        this.driver = driver;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    }
+
+    public void enterUsername(String username) {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(usernameField)).sendKeys(username);
+    }
+
+    public void enterPassword(String password) {
+        driver.findElement(passwordField).sendKeys(password);
+    }
+
+    public void clickLogin() {
+        driver.findElement(loginButton).click();
+    }
+
+    public void login(String username, String password) {
+        enterUsername(username);
+        enterPassword(password);
+        clickLogin();
+    }
+
+    public String getErrorMessage() {
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(errorMessage)).getText();
+    }
+
+    public boolean isErrorDisplayed() {
+        return driver.findElements(errorMessage).size() > 0;
+    }
+}
